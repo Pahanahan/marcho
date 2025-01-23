ec5c0700c9e9637fe40a7e90a5b2ed4b13ba442b
Pavel <korgfd@yandex.ru>
2025-01-23 15:26:22 +0300

    added video-fashion

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,13 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 2000,
   });
+  $('.video-fashion__play').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
 });

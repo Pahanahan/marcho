b5a30306f362080e833b1381009f4083cf6d1121
Pavel <korgfd@yandex.ru>
2025-02-05 15:07:28 +0300

    addaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,10 @@
 $(function () {
-  $(".menu__list-link--active").on("click", function (e) {
-    console.log(e.target);
-  });
-
   $(".menu__btn").on("click", function () {
     $(".menu__list").toggleClass("menu__list--active");
+  });
+
+  $(".shop__filter-btn").on("click", function () {
+    $(".shop__filters").slideToggle();
   });
 
   $(".footer-top__title").on("click", function () {
@@ -45,6 +45,14 @@ $(function () {
     draggable: false,
     arrows: false,
     fade: true,
+    responsive: [
+      {
+        breakpoint: 1051,
+        settings: {
+          draggable: true,
+        }
+      }
+    ]
   });
 
   $(".shop-content__filter-btn").on("click", function () {
@@ -56,9 +64,11 @@ $(function () {
 
   $(".button-list").on("click", function () {
     $(".product-item").addClass("product-item--list");
+    $(".shop-content__inner").addClass("shop-content__nogrid");
   });
   $(".button-grid").on("click", function () {
     $(".product-item").removeClass("product-item--list");
+    $(".shop-content__inner").removeClass("shop-content__nogrid");
   });
 
   $(".select-style").styler();
@@ -93,26 +103,32 @@ $(function () {
       '<svg xmlns="http://www.w3.org/2000/svg"                        xmlns:xlink="http://www.w3.org/1999/xlink"                          width="18px" height="16px" viewBox="0 0 18 16"                      version="1.1"><g id="surface1"><path style="                                stroke: none; fill-rule: nonzero;                                fill-opacity: 1;" d="M 9.902344 0.5625 C 9.738281 0.21875 9.386719 0 9.003906 0 C 8.617188 0 8.273438 0.21875 8.101562 0.5625 L 6.09375 4.695312 L 1.605469 5.359375 C 1.230469 5.414062 0.917969 5.679688 0.804688 6.039062 C 0.6875 6.398438 0.78125 6.792969 1.050781 7.058594 L 4.304688 10.28125 L 3.539062 14.835938 C 3.476562 15.210938 3.632812 15.589844 3.941406 15.8125 C 4.25 16.035156 4.660156 16.0625 4.996094 15.882812 L 9.007812 13.742188 L 13.015625 15.882812 C 13.351562 16.0625 13.761719 16.039062 14.070312 15.8125 C 14.382812 15.585938 14.539062 15.210938 14.476562 14.835938 L 13.703125 10.28125 L 16.960938 7.058594 C 17.226562 6.792969 17.324219 6.398438 17.207031 6.039062 C 17.085938 5.679688 16.777344 5.414062 16.402344 5.359375 L 11.914062 4.695312 Z M 9.902344 0.5625 "/></g></svg>',
   });
 
-  delete L.Icon.Default.prototype._getIconUrl;
+  if (document.querySelector("#map")) {
+    delete L.Icon.Default.prototype._getIconUrl;
 
-  L.Icon.Default.mergeOptions({
-    iconRetinaUrl:
-      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-  });
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl:
+        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+      iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+      shadowUrl:
+        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+    });
 
-  const map = L.map("map").setView([40.71238004031695, -74.00560845746244], 10);
+    const map = L.map("map").setView(
+      [40.71238004031695, -74.00560845746244],
+      10
+    );
 
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
 
-  L.marker([40.71238004031695, -74.00560845746244])
-    .addTo(map)
-    // .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-    .openPopup();
+    L.marker([40.71238004031695, -74.00560845746244])
+      .addTo(map)
+      // .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+      .openPopup();
+  }
 
   if (!document.querySelector(".promo__clock")) return;
 
